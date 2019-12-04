f362675f673964a0f5c0cbc9d147efecb40c7664
Serhy <sergii@status.im>
2019-12-04 14:02:16 +0200

    No touchable-highlihgt in browser tooltip

--- src/status_im/ui/components/tooltip/views.cljs
+++ src/status_im/ui/components/tooltip/views.cljs
@@ -15,12 +15,12 @@
     {:component-did-mount (animations/animate-tooltip bottom-value bottom-anim-value opacity-value -10)}
     [react/view (merge styles/tooltip-container container-style)
      [react/animated-view {:style (styles/tooltip-animated bottom-anim-value opacity-value)}
-      (when label [react/view (styles/tooltip-text-container color)
-                   [react/text {:style (styles/tooltip-text font-size)} label]])
       [vector-icons/icon :icons/tooltip-triangle (assoc
                                                   styles/tooltip-triangle
                                                   :accessibility-label accessibility-label
-                                                  :color color)]]]))
+                                                  :color color)]
+      (when label [react/view (styles/tooltip-text-container color)
+                   [react/text {:style (styles/tooltip-text font-size) :accessibility-label accessibility-label} label]])]]))
 
 (views/defview bottom-tooltip-info [label on-close]
   (views/letsubs [bottom-anim-value (animation/create-value 150)
@@ -34,6 +34,6 @@
                                                   :container-style {:transform [{:rotate "180deg"}]})]
       [react/view styles/bottom-tooltip-text-container
        [react/text {:style styles/bottom-tooltip-text} label]
-       [react/touchable-highlight {:on-press on-close
-                                   :style    styles/close-icon}
-        [vector-icons/icon :main-icons/close {:color colors/white}]]]]]))
+       #_[react/touchable-highlight {:on-press on-close
+                                     :style    styles/close-icon}
+          [vector-icons/icon :main-icons/close {:color colors/white}]]]]]))
